fe7110a5242b9b8498ac09e11829c65533560a8f
Emmett <ekennedy@cps.edu>
2021-07-20 10:24:01 -0500

    Initial Commit

---
+++ Magic Eight Ball 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic Eight Ball 2
+//
+//  Created by Emmett Kennedy on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
