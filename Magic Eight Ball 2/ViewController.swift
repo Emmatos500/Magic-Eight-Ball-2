f66051510d096f5eb5146eb6f2ba47a85d5175f8
Emmett <ekennedy@cps.edu>
2021-07-20 19:48:11 -0500

    Magic Eight Ball 2

--- Magic Eight Ball 2/ViewController.swift
+++ Magic Eight Ball 2/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn(_ sender: Any) {
+        let randomNum = Int.random(in: 0...3)
+        switch randomNum {
+        case 0:
+            ballText.text = "Yes"
+        case 1:
+            ballText.text = "Sorry, no"
+        case 2:
+            ballText.text = "Of course"
+        default:
+            ballText.text = "I don't think so."
+        }
+    }
+    
 }
 
